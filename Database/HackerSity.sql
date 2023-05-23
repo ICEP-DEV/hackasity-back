5cfc7fb2e83e70008311fe59abce1f1d1bfc5aaa
Andries Mathenjwa <andriesmathenjwa52@gmail.com>
2023-05-23 19:07:14 +0200

    database

--- Database/HackerSity.sql
+++ Database/HackerSity.sql
@@ -1,1 +1,199 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 22, 2023 at 11:32 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hackersity`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `Admin_id` int(45) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `surname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `file`
+--
+
+CREATE TABLE `file` (
+  `file_id` int(45) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `size` int(45) NOT NULL,
+  `format` varchar(255) NOT NULL,
+  `date` varchar(255) NOT NULL,
+  `time` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hacker`
+--
+
+CREATE TABLE `hacker` (
+  `stu_no` int(46) NOT NULL,
+  `stu_name` varchar(255) NOT NULL,
+  `stu_surname` varchar(255) NOT NULL,
+  `stu_email` varchar(255) NOT NULL,
+  `group_name` varchar(255) NOT NULL,
+  `stu_password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `judge`
+--
+
+CREATE TABLE `judge` (
+  `Judge_id` int(45) NOT NULL,
+  `Judge_name` varchar(255) NOT NULL,
+  `Judge_surname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `company_name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `presentation`
+--
+
+CREATE TABLE `presentation` (
+  `presentation_id` int(45) NOT NULL,
+  `stu_no` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `team`
+--
+
+CREATE TABLE `team` (
+  `group_id` int(45) NOT NULL,
+  `group_name` varchar(255) NOT NULL,
+  `points` int(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `timeslots`
+--
+
+CREATE TABLE `timeslots` (
+  `slot_id` int(45) NOT NULL,
+  `Judge_id` int(45) NOT NULL,
+  `start_time` varchar(255) NOT NULL,
+  `length` varchar(255) NOT NULL,
+  `end_time` varchar(255) NOT NULL,
+  `date` varchar(255) NOT NULL,
+  `hacker_id` int(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`Admin_id`);
+
+--
+-- Indexes for table `file`
+--
+ALTER TABLE `file`
+  ADD PRIMARY KEY (`file_id`);
+
+--
+-- Indexes for table `hacker`
+--
+ALTER TABLE `hacker`
+  ADD PRIMARY KEY (`stu_no`);
+
+--
+-- Indexes for table `judge`
+--
+ALTER TABLE `judge`
+  ADD PRIMARY KEY (`Judge_id`);
+
+--
+-- Indexes for table `presentation`
+--
+ALTER TABLE `presentation`
+  ADD PRIMARY KEY (`presentation_id`);
+
+--
+-- Indexes for table `timeslots`
+--
+ALTER TABLE `timeslots`
+  ADD PRIMARY KEY (`slot_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `Admin_id` int(45) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `file`
+--
+ALTER TABLE `file`
+  MODIFY `file_id` int(45) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `judge`
+--
+ALTER TABLE `judge`
+  MODIFY `Judge_id` int(45) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `presentation`
+--
+ALTER TABLE `presentation`
+  MODIFY `presentation_id` int(45) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `timeslots`
+--
+ALTER TABLE `timeslots`
+  MODIFY `slot_id` int(45) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
